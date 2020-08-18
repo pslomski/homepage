013302a5cdf065892bf22c03eb3662444712abe1
Piotr Słomski <pslomski@tlen.pl>
2020-08-18 10:02:57 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Piotr';
+const age = 47;
+
+console.log('Hello World!');
+console.log('name=' + firstName);
+console.log('age=' + age);
+console.log(`Nazywam sie ${firstName} i mam ${age} lat`);
